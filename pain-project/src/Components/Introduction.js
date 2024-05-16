0fdd2eade25705428495a05efc94c719c852b265
rashi <rashighosh96@gmail.com>
2024-05-16 13:35:27 -0400

    didactic v1

--- pain-project/src/Components/Introduction.js
+++ pain-project/src/Components/Introduction.js
@@ -1,12 +1,38 @@
 // Introduction.js
-import React from 'react';
+import React, { useState } from 'react';
 import '../CSS/Introduction.css'; // Importing the CSS file
 
 function Introduction() {
+  const [currentContentIndex, setCurrentContentIndex] = useState(0);
+  const contentItems = [
+    { name: 'Pain Disparities', src: '/videos/PainDisparities.mp4' },
+    { name: 'Empathy', src: '/videos/Empathy.mp4' },
+    { name: 'Perspective Taking', src: '/videos/PerspectiveTaking.mp4' },
+  ];
+
+  const handleNext = () => {
+    setCurrentContentIndex((prevIndex) => (prevIndex + 1) % contentItems.length);
+  };
+
+  const handlePrevious = () => {
+    setCurrentContentIndex((prevIndex) => (prevIndex - 1 + contentItems.length) % contentItems.length);
+  };
+
+
+
   return (
     <div className='introduction'>
-      <h1>Welcome to the Introduction Page</h1>
-      <p>This is the introduction page of our Pain Project.</p>
+      <h1>Introduction to {contentItems[currentContentIndex].name}</h1>
+      <p className="description">Daren, a virtual physician "expert," will give a brief talk pain disparities, perspective-taking, and empathy. Click the video below to begin!</p>
+      <br/>
+      <video key={contentItems[currentContentIndex].src} height="650" controls>
+          <source src={contentItems[currentContentIndex].src} type="video/mp4" />
+          Your browser does not support the video tag.
+      </video> 
+      <div className='button-area'>
+        {currentContentIndex != 0 && <button className='previous' onClick={handlePrevious}>◄ Previous: {contentItems[currentContentIndex-1].name}</button> }
+        {currentContentIndex != contentItems.length-1 && <button className='next' onClick={handleNext}>Next: {contentItems[currentContentIndex+1].name} ►</button> }
+      </div>
     </div>
   );
 }
