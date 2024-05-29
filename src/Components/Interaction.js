1da39ab534be56e124b631297a6dac47e17a9317
rashi <rashighosh96@gmail.com>
2024-05-29 19:28:00 -0400

    made chat interface

--- src/Components/Interaction.js
+++ src/Components/Interaction.js
@@ -1,13 +1,26 @@
 // Interaction.js
 import React from 'react';
 import '../CSS/Interaction.css'; // Importing the CSS file
-
+import PatientInfoToggle from '../Helpers/PatientInfoToggle';
 
 function Interaction() {
   return (
-    <div className="interaction">
-      <h1>Welcome to the Interaction Page</h1>
-      <p>This is the interaction page of our Pain Project.</p>
+      <div className="video-background">
+      <video autoPlay muted loop>
+        <source id="myVideo"  src="https://painproject-content.s3.amazonaws.com/rhonda-moore-videos/BF_IDLE.mp4" type="video/mp4" />
+        {/* Add additional source elements for different video formats */}
+        Your browser does not support the video tag.
+      </video>
+      <div className="content-overlay">
+        {/* Add your content here */}
+        <PatientInfoToggle />
+        <df-messenger
+          intent="WELCOME"
+          chat-title="Patient: Rhonda Moore"
+          agent-id="933558cf-b5c2-455e-8910-c52a1c14ea07"
+          language-code="en"
+        ></df-messenger>
+      </div>
     </div>
   );
 }
