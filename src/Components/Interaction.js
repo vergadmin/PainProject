23fe9af27c896fd810183eec0b60f1613586cda5
rashi <rashighosh96@gmail.com>
2024-05-30 13:07:33 -0400

    using refs hooks and callbacks to avoid warnings and rerenders

--- src/Components/Interaction.js
+++ src/Components/Interaction.js
@@ -1,64 +1,59 @@
 // Interaction.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import '../CSS/Interaction.css'; // Importing the CSS file
 import PatientInfoToggle from '../Helpers/PatientInfoToggle';
 
 function Interaction() {
-  const [responseVideoSrc, setResponseVideoSrc] = useState('responseVideo');
-  const [responseVideoVisibility, setResponseVideoVisibility] = useState('hide')
-  const [idleVideoVisibility, setIdleVideoVisibility] = useState('show')
-
+  const [responseVideoSrc, setResponseVideoSrc] = useState('');
+  const [responseVideoVisibility, setResponseVideoVisibility] = useState('hide');
+  const [idleVideoVisibility, setIdleVideoVisibility] = useState('show');
   const [isMinimized, setIsMinimized] = useState(false);
+
+  const responseVideoRef = useRef(null);
+  const idleVideoRef = useRef(null);
+  const dfMessengerRef = useRef(null);
 
   const toggleMinimize = (toggle) => {
     setIsMinimized(toggle);
   };
 
-  // Function to handle video source change
-  const changeVideoSource = (newSrc) => {
-    console.log("TRIGGERED", newSrc)
+  const changeVideoSource = useCallback((newSrc) => {
+    console.log("TRIGGERED", newSrc);
     setResponseVideoSrc(newSrc);
-    setResponseVideoVisibility('show')
-    setIdleVideoVisibility('hide')
-    console.log("RESPONSE VIDEO:", responseVideo)
-    console.log("IDLE VIDEO:", idleVideo)
-    toggleMinimize(true)
-  };
-
-  const switchToIdle = () => {
-    console.log("SWITCHING BACK TO IDLE")
-    setResponseVideoVisibility('hide')
-    setIdleVideoVisibility('show')
-    toggleMinimize(false)
-  };
-
-  var responseVideo = ''
-  var idleVideo = ''
-  var dfMessenger = ''
-
-  useEffect(() => {
-    responseVideo = document.getElementById("responseVideo");
-    idleVideo = document.getElementById("idleVideo");
-    dfMessenger = document.querySelector('df-messenger'); 
-    fetchSynthesiaVideo()
+    setResponseVideoVisibility('show');
+    setIdleVideoVisibility('hide');
+    console.log("RESPONSE VIDEO:", responseVideoRef.current);
+    console.log("IDLE VIDEO:", idleVideoRef.current);
+    toggleMinimize(true);
   }, []);
 
-  const fetchSynthesiaVideo = () => {
-    //find dfMessenger declared in html file
-    dfMessenger.addEventListener('df-response-received', function (event) {
-      var displayName = event.detail.response.queryResult.intent.displayName;
-      //make AWS video link
+  const switchToIdle = () => {
+    console.log("SWITCHING BACK TO IDLE");
+    setResponseVideoVisibility('hide');
+    setIdleVideoVisibility('show');
+    toggleMinimize(false);
+  };
+
+  const fetchSynthesiaVideo = useCallback(() => {
+    dfMessengerRef.current.addEventListener('df-response-received', (event) => {
+      let displayName = event.detail.response.queryResult.intent.displayName;
       displayName = displayName.replaceAll(" ", "");
       displayName = displayName.replaceAll("/", "_");
       console.log(displayName);
-      //change this to link to AWS Repo for scenario videos
-      var AWS_videoURL_Base = "https://painproject-content.s3.amazonaws.com/rhonda-moore-videos/";
-      var videoURL = AWS_videoURL_Base + displayName + ".mp4";
-      if (displayName !=="DefaultWelcomeIntent") {
-        changeVideoSource(videoURL)
+      const AWSVideoURLBase = "https://painproject-content.s3.amazonaws.com/rhonda-moore-videos/";
+      const videoURL = `${AWSVideoURLBase}${displayName}.mp4`;
+      if (displayName !== "DefaultWelcomeIntent") {
+        changeVideoSource(videoURL);
       }
     });
-  }
+  }, [changeVideoSource]);
+
+  useEffect(() => {
+    responseVideoRef.current = document.getElementById("responseVideo");
+    idleVideoRef.current = document.getElementById("idleVideo");
+    dfMessengerRef.current = document.querySelector('df-messenger');
+    fetchSynthesiaVideo();
+  }, [fetchSynthesiaVideo]);
 
   return (
       <div className="video-background">
