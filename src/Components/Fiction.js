93b3760c3bcbba786323b5774a564d98d39860b6
rghosh96 <rashighosh96@gmail.com>
2024-06-28 08:56:43 -0400

    updated fiction contract

--- src/Components/Fiction.js
+++ src/Components/Fiction.js
@@ -6,20 +6,18 @@ import '../CSS/Fiction.css'; // Importing the CSS file
 
 const Fiction = () => {
   const contentItems = [
-    { title: "Training Developer Responsibilities", subTitle: "What will we do?", content: ["Create goal-oriented, practical simulations based on measurable learning objectives.", "Add enough realism to each simulation so the learner receives enough clues to identify and solve a problem.", "Set and maintain an engaging learning environment.", "Foster reflective practice."]},
+    { title: "Training Developer Responsibilities", subTitle: "What will the training developers do?", content: ["Create goal-oriented, practical simulations based on measurable learning objectives.", "Add enough realism to each simulation so the learner receives enough clues to identify and solve a problem.", "Set and maintain an engaging learning environment.", "Foster reflective practice."]},
     { title: "Learner Responsibilities", subTitle: "What do I need to do?", content: ["Suspend judgment of realism for any given simulation in exchange for the promise of learning new and reinforcing existing knowledge and skills.", "Maintain a genuine desire to learn even when suspending disbelief becomes difficult.", "Treat the simulated patients with the same care and respect as an actual patient."] },
-    { title: "Top 3 Things to Remember", subTitle: "What should I remember?", content: ["Our Basic Assumption: 'We believe that everyone participating in these sessions is intelligent, capable, cares about doing their best, and wants to improve how they care for patients.'", "Areas of strengths and weakness identified by these simulations are intended as constructive feedback, not criticism.", "The 'Vegas' Rule – What happens here, stays here. Your responses will NOT be shared outside of this session. All results will be anonymized and pooled before disseminating them to the broader scholarly community."]},
+    { title: "Top 3 Things to Remember", subTitle: "What should I remember?", content: ["Our Basic Assumption: “We believe that everyone participating in these sessions is intelligent, capable, cares about doing their best, and wants to improve how they care for patients.”", "Areas of strengths and weakness identified by these simulations are intended as constructive feedback, not criticism.", "The “Vegas” Rule – What happens here, stays here. Your responses will NOT be shared outside of this session. All results will be anonymized and pooled before disseminating them to the broader scholarly community."]},
     { title: "Are you ready to begin?", subTitle: "", content: ["I'm ready to begin!"]}
 ];
 
     const headerItems = [
         {header: "Healthcare Simulation Fiction Contract", subText1: "The purpose of simulation-based training in healthcare is to help participants solidify their skills for working with actual patients. The developers of this training have recreated realistic patient care situations using patient simulations. The realism of each simulation may vary depending on the learning goals for the session. The simulated patients and environment have certain limitations in their ability to mirror real life exactly.", subText2: "When participating in these simulations, you assume all aspects of a practicing healthcare clinician’s professional behavior. Additionally, when a gap occurs between simulated reality and actual reality, you are expected to try to understand the goals of the learning session and behave accordingly."},
         {header: "Please read the following before beginning.", subText1: "In the next section, you will interact with two virtual patients with chronic pain. Your goal for these interactions is to understand better how pain can affect your patients’ lives from their perspective. You will have up to 10 minutes to interact with each patient. You don’t have to use the entire 10 minutes, but you should aim to spend enough time with each patient to consider their lived experience.", subText2: "You may ask them specific questions about their pain, when it started, its severity, and any treatments they have tried. However, for this training session, try to focus your conversation more on how their pain is affecting different aspects of their lives, such as their family, work, and daily routine. Try practicing the empathy and perspective-taking techniques introduced in this training."}
-        
     ]
 
   const [currentContentIndex, setCurrentContentIndex] = useState(0);
-  const [currentHeaderIndex, setCurrentHeaderIndex] = useState(0);
 
   const [checkedItems, setCheckedItems] = useState(new Array(contentItems[0].content.length).fill(false));
   const navigate = useNavigate();
@@ -35,12 +33,7 @@ const Fiction = () => {
   const handleNext = () => {
     if (checkedItems.every(item => item)) {
       const nextIndex = currentContentIndex + 1;
-      if (nextIndex === 2) { // 3
-        const nextHeaderIndex = currentHeaderIndex + 1;
-        setCheckedItems(new Array(contentItems[nextIndex].content.length).fill(false)); // Reset checkboxes for the next item
-        setCurrentContentIndex(nextIndex);
-        setCurrentHeaderIndex(nextHeaderIndex);
-    } else if (nextIndex >= contentItems.length) {
+      if (nextIndex >= contentItems.length) {
             navigate('/interaction'); // Navigate to /interaction after all content items
      } else {
         setCheckedItems(new Array(contentItems[nextIndex].content.length).fill(false)); // Reset checkboxes for the next item
@@ -52,7 +45,7 @@ const Fiction = () => {
   const canProceed = checkedItems.every(item => item);
 
   const { title, subTitle, content } = contentItems[currentContentIndex];
-  const { header, subText1, subText2} = headerItems[currentHeaderIndex];
+  const { header, subText1, subText2} = headerItems[0];
 
   return (
     <div className = 'fiction'>
